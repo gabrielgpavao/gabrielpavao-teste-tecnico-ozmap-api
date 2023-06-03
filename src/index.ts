eb50a8afaaa002967029162284a158dea46d4b10
Gabriel Pavão <gabpavao@outlook.com>
2023-06-03 13:49:12 -0300

    feat: middleware to catch global errors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import Koa from 'koa';
 import Router from 'koa-router';
+import { handleErrorsMiddleware } from './middlewares/errors.middleware'
 
 const app = new Koa();
 const router = new Router();
 
+app.use(handleErrorsMiddleware);
 app.use(router.routes()).use(router.allowedMethods());
 
 export default app;
