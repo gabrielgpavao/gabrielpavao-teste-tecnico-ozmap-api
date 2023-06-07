6ce4dac4d911a25df7f8a6cffe3e974490bce971
Gabriel Pavão <gabpavao@outlook.com>
2023-06-07 15:53:31 -0300

    feat: set up database to run unit tests

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,10 +1,24 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import path from 'path';
+import 'dotenv/config';
 
 function dataSourceConfig (): DataSourceOptions {
 	const entitiesPath: string = path.join(__dirname, './entities/**.{ts,js}');
-	const migrationsPath: string = path.join(__dirname, './migrations/**.{ts,js}');
 	const databasePath: string = path.join(__dirname, '../database.db');
+
+	const nodeEnv: string | undefined = process.env.NODE_ENV;
+
+
+	if(nodeEnv === 'test'){
+		return {
+			type: 'sqlite',
+			database: ':memory:',
+			synchronize: true,
+			entities: [entitiesPath]
+		};
+	}
+
+	const migrationsPath: string = path.join(__dirname, './migrations/**.{ts,js}');
 
 	return {
 		type: 'sqlite',
