ba42d3e0eb845763d8518b19a0f03e22fdb669aa
Gabriel Pavão <gabpavao@outlook.com>
2023-06-07 16:04:05 -0300

    test: errors expected when trying to create an invalid user

--- test/unit/users/createUser.spec.ts
+++ test/unit/users/createUser.spec.ts
@@ -50,4 +50,70 @@ describe('POST /users', () => {
 				done();
 			});
 	});
+
+	it('Error: Must not be able to create an user - Name already exists', async () => {
+		await userRepo.save(createUserMock.uniqueNameUser);
+
+		const res = await chai.request(app)
+			.post(baseUrl)
+			.send(createUserMock.nameAlreadyExistsUser);
+
+		expect(res).to.have.status(409);
+		expect(res.body).to.deep.equal({
+			code: 409,
+			message: 'Name already exists'
+		});
+	});
+
+	it('Error: Must not be able to create an user - Email already exists', async () => {
+		await userRepo.save(createUserMock.uniqueEmailUser);
+
+		const res = await chai.request(app)
+			.post(baseUrl)
+			.send(createUserMock.emailAlreadyExistsUser);
+
+		expect(res).to.have.status(409);
+		expect(res.body).to.deep.equal({
+			code: 409,
+			message: 'Email already exists'
+		});
+	});
+
+	it('Error: Must not be able to create an user - Invalid Age', (done) => {
+		chai.request(app)
+			.post(baseUrl)
+			.send(createUserMock.invalidAgeUser)
+			.end(function (err, res) {
+				expect(err).to.be.null;
+				expect(res).to.have.status(400);
+				expect(res.body).to.deep.equal({
+					age: [
+						'Number must be greater than or equal to 18'
+					]
+				});
+				done();
+			});
+	});
+
+	it('Error: Must not be able to create an user - Invalid Body', (done) => {
+		chai.request(app)
+			.post(baseUrl)
+			.send(createUserMock.invalidBodyUser)
+			.end(function (err, res) {
+				expect(err).to.be.null;
+				expect(res).to.have.status(400);
+				expect(res.body).to.deep.equal({
+					name: [
+						'Expected string, received boolean'
+					],
+					email: [
+						'Invalid email'
+					],
+					age: [
+						'Expected number, received string'
+					]
+				});
+				done();
+			});
+	});
 });
