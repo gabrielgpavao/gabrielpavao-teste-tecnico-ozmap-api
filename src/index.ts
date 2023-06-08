1215928d095f928583260d97422207e4b98dd6af
Gabriel Pavão <gabpavao@outlook.com>
2023-06-08 10:05:44 -0300

    feat: enable cors

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import Koa from 'koa';
 import { handleErrorsMiddleware } from './middlewares/errors.middleware';
 import { usersRoutes } from './routes/users.routes';
 import { docsRoutes } from './routes/docs.routes';
+import cors from 'kcors';
 
 const app: Koa = new Koa();
+
+app.use(cors({ origin: '*' }));
 
 app.use(docsRoutes.routes()).use(docsRoutes.allowedMethods());
 
